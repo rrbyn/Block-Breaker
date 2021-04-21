fa5e15dca64e7f20bc72e466b2d0d7c51c8448a8
rrbyn <robin.kukk23@gmail.com>
2021-04-21 08:19:55 +0300

    First commit

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Block : MonoBehaviour
+{
+    // params
+    [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockSparklesVFX;
+    [SerializeField] Sprite[] hitSprites;
+
+    //cached reference
+    Level level;
+
+    // state variables
+    [SerializeField] int timesHit; // debug
+
+    private void Start()
+    {
+        CountBreakableBlocks();
+    }
+
+    private void CountBreakableBlocks()
+    {
+        level = FindObjectOfType<Level>();
+        if (tag == "Breakable")
+        {
+            level.CountBlocks();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    { if (tag == "Breakable")
+        {
+            HandleHit();
+        }
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+        int maxHits = hitSprites.Length + 1;
+        if (timesHit >= maxHits)
+        {
+            DestroyBlock();
+        }
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+        if (hitSprites[spriteIndex] != null)
+        {
+            GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
+        else
+        {
+            Debug.LogError("machine broke in " + gameObject.name);
+        }
+    }
+
+    private void DestroyBlock()
+    {  
+            PlayBlockDestroySFX();
+            Destroy(gameObject, 0.0f);
+            level.DestroyBreakableBlocks();
+            TriggerSparklesVFX();
+    }
+
+    private void PlayBlockDestroySFX()
+    {
+        FindObjectOfType<GameSession>().AddToScore();
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+    }
+
+    private void TriggerSparklesVFX()
+    {
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+        Destroy(sparkles, 1f);
+    }
+
+
+}
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+    
+{
+    // parameters
+    [SerializeField] int breakableBlocks; // debug
+
+    // cache
+    SceneLoader sceneloader;
+
+    private void Start()
+    {
+        sceneloader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void CountBlocks()
+    {
+        breakableBlocks++;
+    }
+
+    public void DestroyBreakableBlocks()
+    {
+        breakableBlocks--;
+        if (breakableBlocks <= 0)
+        {
+            sceneloader.LoadNextScene();
+        }
+
+    }
+
+    public void Update()
+    {
+            
+    }
+
+
+}
